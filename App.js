f55f03e26050b8b31c3dfec331799bef7c4f6dc1
Terhi Repo <t0rete00@students.oamk.fi>
2024-02-05 16:45:14 +0200

    MapView project

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, SafeAreaView } from 'react-native';
+import Map from './screens/Map';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Map />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
